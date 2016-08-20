a0bfcd424b0097226fce636703c6ab7cb5f8c460
Fu Yong Quah <quah.fy95@gmail.com>
2016-08-19 23:08:55 -0700

    Add product view

--- products/urls.py
+++ products/urls.py
@@ -3,15 +3,16 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-
-from .views import ProductDetailView, ProductListView,VariationListView 
+from products import views
 
 urlpatterns = [
+    url(r'^search/$', views.ProductSearchView.as_view(), name='products_search'),
+    url(r'^$', views.ProductListView.as_view(), name='products'),
+    url(r'^(?P<pk>\d+)/$', views.ProductDetailView.as_view(), name='product_detail'),
+    url(r'^(?P<pk>\d+)/inventory/$', views.VariationListView.as_view(), name='product_inventory'),
+]
+
     # Examples:
     # url(r'^$', 'newsletter.views.home', name='home'),
     #url(r'^$', 'products.views.product_list', name='products'),
-    url(r'^$', ProductListView.as_view(), name='products'),
-    url(r'^(?P<pk>\d+)/$', ProductDetailView.as_view(), name='product_detail'),
-    url(r'^(?P<pk>\d+)/inventory/$', VariationListView.as_view(), name='product_inventory'),
     #url(r'^(?P<id>\d+)', 'products.views.product_detail_view_func', name='product_detail_function'),
-]
